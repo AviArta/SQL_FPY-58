e6110283e25a16eb717afad9251247c5e707d1ce
AviArta <buzlova.a@yandex.ru>
2022-08-16 21:51:48 +0300

    Доработка ДЗ 2

--- HW_SQL_2.1_create.sql
+++ HW_SQL_2.1_create.sql
@@ -1,32 +1,35 @@
 create table if not exists genres_of_music (
 	id_genre SERIAL primary key,
-	name_genre text not null
+	name_genre VARCHAR (20) UNIQUE not null
 );
 
 create table if not exists artists (
 	id_artist SERIAL primary key,
-	name_artist text unique not null,
-	genre_artist integer not null references genres_of_music(id_genre)
+	name_artist VARCHAR (20) unique not null
 );
+-- genre_artist integer not null references genres_of_music(id_genre)
 
 create table if not exists albums (
 	id_album SERIAL primary key,
-	title_album text not null,
+	title_album VARCHAR (20) not null,
 	release_year_album smallint not null,
-	name_artist_album integer not null references artists(id_artist)
+	check (release_year_album>1800)
 );
+-- name_artist_album integer not null references artists(id_artist)
 
 create table if not exists tracks (
 	id_track SERIAL primary key,
-	name_track text not null,
+	name_track VARCHAR (20) not null,
 	duration_track integer not null,
-	album_track integer not null references albums(id_album)
+	album_track integer not null references albums(id_album),
+	check (duration_track<2000)
 );
 
 create table if not exists collections (
 	id_collection SERIAL primary key,
-	title_collection text not null,
+	title_collection VARCHAR (20) not null,
 	release_year_collection integer not null
+	check (release_year_collection>1990)
 );
 
 create table if not exists genres_artists (
